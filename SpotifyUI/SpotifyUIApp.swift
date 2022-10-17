7de2ab3f54fb3615b04483fbb90c8e773ba85524
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-17 19:03:57 +0300

    Initial Commit

---
+++ SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by Amby on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
